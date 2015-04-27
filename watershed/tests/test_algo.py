4063e7eca60fdcc999e1654d325dcbfa944a2976
Paul Hobson <pmhobson@gmail.com>
2015-04-26 22:57:27 -0700

    move ESRI example data to testing.testing

--- watershed/tests/test_algo.py
+++ watershed/tests/test_algo.py
@@ -1,61 +1,11 @@
 import numpy
+from scipy import ndimage
 
 import nose.tools as nt
 import numpy.testing as nptest
-from scipy import ndimage
 
 from watershed import algo
-
-
-## ESRI Example Dataset
-## see: http://goo.gl/UeiaCi
-ESRI_TOPO = numpy.array([
-    [78., 72., 69., 71., 58., 49.],
-    [74., 67., 56., 49., 46., 50.],
-    [69., 53., 44., 37., 38., 48.],
-    [64., 58., 55., 22., 31., 24.],
-    [68., 61., 47., 21., 16., 19.],
-    [74., 53., 34., 12., 11., 12.],
-])
-
-ESRI_FLOW_DIR_D8 = numpy.array([
-    [  2,   2,   2,   4,   4,   8],
-    [  2,   2,   2,   4,   4,   8],
-    [  1,   1,   2,   4,   8,   4],
-    [128, 128,   1,   2,   4,   8],
-    [  2,   2,   1,   4,   4,   4],
-    [  1,   1,   1,   1,   4,  16]
-])
-
-ESRI_UPSTREAM_HIGH = numpy.array([
-    [0, 1, 1, 1, 0, 0],
-    [0, 0, 1, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-])
-
-ESRI_UPSTREAM_LOW = numpy.array([
-    [1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-])
-
-## modified from http://goo.gl/Pzvuvz
-## per http://goo.gl/PZWbOE
-ESRI_FLOW_ACC = numpy.array([
-    [ 0,  0,  0,  0,  0,  0],
-    [ 0,  1,  1,  2,  2,  0],
-    [ 0,  3,  7,  5,  4,  0],
-    [ 0,  0,  0, 20,  0,  1],
-    [ 0,  0,  0,  1, 24,  0],
-    [ 0,  2,  4,  7, 35,  1],
-])
-
+from watershed.testing import testing
 
 def test__stack_neighbors():
     arr = numpy.arange(6).reshape(2, 3)
@@ -117,8 +67,8 @@ class baseFlowDir_Mixin(object):
 
 class test_flow_direction_arcgis(baseFlowDir_Mixin):
     def setup(self):
-        self.topo = ESRI_TOPO.copy()
-        self.known_flow_dir_d8 = ESRI_FLOW_DIR_D8.copy()
+        self.topo = testing.ESRI_TOPO.copy()
+        self.known_flow_dir_d8 = testing.ESRI_FLOW_DIR_D8.copy()
 
 
 ##
@@ -135,12 +85,12 @@ class baseTraceUpstream_Mixin(object):
 
 class test_trace_upstream_arcgis(baseTraceUpstream_Mixin):
     def setup(self):
-        self.flow_dir = ESRI_FLOW_DIR_D8.copy()
+        self.flow_dir = testing.ESRI_FLOW_DIR_D8.copy()
         self.row_high, self.col_high = (2, 3)
-        self.known_upstream_high = ESRI_UPSTREAM_HIGH.copy()
+        self.known_upstream_high = testing.ESRI_UPSTREAM_HIGH.copy()
 
         self.row_low, self.col_low = (3, 3)
-        self.known_upstream_low = ESRI_UPSTREAM_LOW.copy()
+        self.known_upstream_low = testing.ESRI_UPSTREAM_LOW.copy()
 
 
 ##
@@ -164,25 +114,25 @@ class test_mask_upstream_arcgis(baseMaskUpstream_Mixin):
     def setup(self):
         self.factor = 1.
         self.order = 0
-        self.topo = ESRI_TOPO.copy()
+        self.topo = testing.ESRI_TOPO.copy()
         self.row_high, self.col_high = (2, 3)
-        self.known_upstream_high = ESRI_UPSTREAM_HIGH.copy()
+        self.known_upstream_high = testing.ESRI_UPSTREAM_HIGH.copy()
 
         self.row_low, self.col_low = (3, 3)
-        self.known_upstream_low = ESRI_UPSTREAM_LOW.copy()
+        self.known_upstream_low = testing.ESRI_UPSTREAM_LOW.copy()
 
 
 class test_mask_upstream_arcgis_zoomed(baseMaskUpstream_Mixin):
     def setup(self):
         self.factor = 2.
         self.order = 0
-        self.topo = ndimage.zoom(ESRI_TOPO.copy(), self.factor, order=self.order)
+        self.topo = ndimage.zoom(testing.ESRI_TOPO.copy(), self.factor, order=self.order)
         self.row_high, self.col_high = (2*self.factor, 3*self.factor)
-        self.known_upstream_high = ndimage.zoom(ESRI_UPSTREAM_HIGH.copy(),
+        self.known_upstream_high = ndimage.zoom(testing.ESRI_UPSTREAM_HIGH.copy(),
                                                 self.factor, order=self.order)
 
         self.row_low, self.col_low = (3*self.factor, 3*self.factor)
-        self.known_upstream_low = ndimage.zoom(ESRI_UPSTREAM_LOW.copy(),
+        self.known_upstream_low = ndimage.zoom(testing.ESRI_UPSTREAM_LOW.copy(),
                                                self.factor, order=self.order)
 
 
@@ -196,6 +146,6 @@ class baseFlowAccumulation_Mixin(object):
 
 class test_flow_accumulation_arcgis(baseFlowAccumulation_Mixin):
     def setup(self):
-        self.flow_dir = ESRI_FLOW_DIR_D8.copy()
-        self.known_flow_acc = ESRI_FLOW_ACC.copy()
+        self.flow_dir = testing.ESRI_FLOW_DIR_D8.copy()
+        self.known_flow_acc = testing.ESRI_FLOW_ACC.copy()
 
