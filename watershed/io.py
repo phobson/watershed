621a997350770931e56b0f9bcf9b1cb5ce03c0c9
Paul Hobson <pmhobson@gmail.com>
2015-11-30 20:05:15 -0800

    minor docstring tweaks

--- watershed/io.py
+++ watershed/io.py
@@ -2,6 +2,7 @@ from __future__ import division
 
 import sys
 import os
+from pkg_resources import resource_filename
 
 import matplotlib.image as mplimg
 
@@ -14,6 +15,6 @@ def load_example(example_name):
     if example_name not in EXAMPLES:
         raise ValueError("`examples_name` must be in {}".format(EXAMPLES))
 
-    filename = os.path.join(sys.prefix, 'watershed_data', 'testing', example_name + '.png')
+    filename = resource_filename('watershed.testing.data', example_name + '.png')
     return mplimg.imread(filename)
 
